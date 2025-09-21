bd53f561fd315d73c051381a2f42fda74cb55bf3
Temppa127 <temppa127@gmail.com>
2025-09-21 23:35:18 +0300

    Update register.js

--- src/register.js
+++ src/register.js
@@ -1,4 +1,4 @@
-import { AWW_COMMAND, INVITE_COMMAND, PROMPT_COMMAND } from './commands.js';
+import { AWW_COMMAND, CHANNEL_COMMAND, INVITE_COMMAND, PROMPT_COMMAND } from './commands.js';
 import dotenv from 'dotenv';
 import process from 'node:process';
 
@@ -34,7 +34,7 @@ const response = await fetch(url, {
     Authorization: `Bot ${token}`,
   },
   method: 'PUT',
-  body: JSON.stringify([AWW_COMMAND, INVITE_COMMAND, PROMPT_COMMAND]),
+  body: JSON.stringify([AWW_COMMAND, INVITE_COMMAND, PROMPT_COMMAND, CHANNEL_COMMAND]),
 });
 
 if (response.ok) {
