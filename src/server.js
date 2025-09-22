6e336785c7540f35de78038100c705b49b300b73
Temppa127 <temppa127@gmail.com>
2025-09-22 17:01:10 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -72,7 +72,7 @@ router.post('/', async (request, env) => {
           type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
           data: {
             content:
-              res
+              JSON.stringify(res)
           },
         });
       }
